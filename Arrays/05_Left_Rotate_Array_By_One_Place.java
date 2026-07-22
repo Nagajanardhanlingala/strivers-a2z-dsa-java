605649f12053598cb1f8f2c023bd52f479148afa
Nagajanardhanlingala <nanilingala07@gmail.com>
2026-07-22 20:29:19 +0530

    Complete Striver A2Z Arrays: Left Rotate by One Place

---
+++ Arrays/05_Left_Rotate_Array_By_One_Place.java
@@ -0,0 +1,17 @@
+class Main{
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5};
+        int n = arr.length;
+        int temp = arr[0];
+
+        for(int i=0;i<n-1;i++){
+            arr[i] = arr[i+1];
+        }
+
+        arr[n-1] = temp;
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+    }
+}
