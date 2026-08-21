ef741037c16765658da98827c53f41b75d4b5e1a
Nagajanardhanlingala <nanilingala07@gmail.com>
2026-08-21 11:40:33 +0530

    Complete Binary Search on 1D Arrays

---
+++ Binary Search/11_FindNumberOfRotations.java
@@ -0,0 +1,20 @@
+class Main{
+    public static int findRotations(int[] arr){
+        int low = 0;
+        int high = arr.length-1;
+        while(low < high){
+            int mid = low+(high-low)/2;
+            if(arr[mid] > arr[high]){
+                low = mid+1;
+            }else{
+                high=mid;
+            }
+        }
+        return low;
+    }
+    
+    public static void main(String[] args){
+        int[] arr={4,5,6,7,0,1,2,3};
+        System.out.print(findRotations(arr));
+    }
+}
