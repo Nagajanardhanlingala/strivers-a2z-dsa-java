5f58bbfd43063a2545c430286afcf8ec12b1fa8e
Nagajanardhanlingala <nanilingala07@gmail.com>
2026-09-02 13:38:46 +0530

    Complete Binary Search Advanced Problems - Median and Kth Element

---
+++ Binary Search/27_KthElementofTwoSortedArrays.java
@@ -0,0 +1,31 @@
+class Main{
+    public static int kthElement(int[] arr1,int[] arr2,int k){
+        if(arr1.length > arr2.length){
+            return kthElement(arr2,arr1,k);
+        }
+        int m = arr1.length;
+        int n = arr2.length;
+        
+        int low =Math.max(0,k-n);
+        int high=Math.min(k,m);
+        while(low <= high){
+            int cut1 = low+(high-low)/2;
+            int cut2 = k-cut1;
+            
+            int l1 =(cut1 == 0) ? Integer.MIN_VALUE : arr1[cut1-1];
+            int r1 =(cut1 == m) ? Integer.MAX_VALUE : arr1[cut1];
+            
+            int l2 =(cut2 == 0) ? Integer.MIN_VALUE : arr2[cut2 -1];
+            int r2 =(cut2 == n) ? Integer.MAX_VALUE : arr2[cut2];
+            
+            if(l1 <= r2 && l2 <= r1){
+                return Math.max(l1,l2);
+            }else if(l1 > r2){
+                high = cut1-1;
+            }else{
+                low = cut1 + 1;
+            }
+        }
+        return -1;
+    }
+}
